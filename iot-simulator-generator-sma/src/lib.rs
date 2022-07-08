908e00174ffc64f8a11425a8948c4e636817655e
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2022-07-08 21:56:54 +0200

    Implement SMA generator plugin

--- iot-simulator-generator-sma/src/lib.rs
+++ iot-simulator-generator-sma/src/lib.rs
@@ -8,7 +8,7 @@ use iot_simulator_api::generator::*;
 pub fn new_instance(
     min: f32,
     max: f32,
-    precision: u8,
+    precision: u32,
     buffer_size: usize,
 ) -> Box<dyn StatefulGeneratorPlugin<(), f32>> {
     SMAGenerator::new(min, max, precision, buffer_size)
@@ -17,17 +17,19 @@ pub fn new_instance(
 pub struct SMAGenerator {
     min: f32,
     max: f32,
-    precision: u8,
-    buffer: VecDeque<i32>,
+    precision: u32,
+    buffer_size: u8,
+    buffer: VecDeque<f32>,
     rng: ThreadRng,
 }
 
 impl SMAGenerator {
-    fn new(min: f32, max: f32, precision: u8, buffer_size: usize) -> Box<SMAGenerator> {
+    fn new(min: f32, max: f32, precision: u32, buffer_size: usize) -> Box<SMAGenerator> {
         Box::new(SMAGenerator {
             min,
             max,
             precision,
+            buffer_size: buffer_size as u8,
             buffer: VecDeque::with_capacity(buffer_size),
             rng: rand::thread_rng(),
         })
@@ -37,8 +39,24 @@ impl SMAGenerator {
 impl GeneratorPlugin<(), f32> for SMAGenerator {
     fn generate(&mut self, _: ()) -> f32 {
         let val: f32 = self.rng.gen_range(self.min..self.max);
-        val
+        if self.buffer.is_empty() {
+            for _ in 1..=self.buffer_size {
+                self.buffer.push_front(val)
+            }
+        }
+        self.buffer.pop_back();
+        self.buffer.push_front(val);
+        round(avg(&mut self.buffer), self.precision)
     }
+}
+
+fn avg(buffer: &mut VecDeque<f32>) -> f32 {
+    buffer.iter().sum::<f32>() / buffer.len() as f32
+}
+
+fn round(n: f32, precision: u32) -> f32 {
+    let p = 10i32.pow(precision) as f32;
+    (n * p).round() / p
 }
 
 impl StatefulGeneratorPlugin<(), f32> for SMAGenerator {}
--- iot-simulator-generator-sma/src/lib_tests.rs
+++ iot-simulator-generator-sma/src/lib_tests.rs
@@ -1,12 +1,43 @@
+use crate::avg;
+use std::collections::VecDeque;
+
 #[cfg(test)]
 mod lib_tests {
-    use super::super::{*};
+    use super::super::*;
+    use crate::lib_tests::std_deviation;
 
     #[test]
-    fn it_should_generate() {
-        let mut plugin = new_instance(10.0, 20.0, 3, 5);
+    fn it_should_generate_in_the_range() {
+        let mut plugin = new_instance(10.0, 20.0, 2, 20);
 
-        assert!(plugin.generate(()) > 10.0);
-        assert!(plugin.generate(()) < 20.0);
+        for _ in 1..150 {
+            let val = plugin.generate(());
+            assert!(val > 10.0 && val < 20.0);
+        }
     }
+
+    #[test]
+    fn it_should_have_lower_std_when_the_bucket_is_bigger() {
+        let mut plugin_small = new_instance(10.0, 20.0, 2, 5);
+        let mut plugin_large = new_instance(10.0, 20.0, 2, 20);
+
+        let mut small_vals: VecDeque<f32> = (1..150).map(|_| plugin_small.generate(())).collect();
+        let mut large_vals: VecDeque<f32> = (1..150).map(|_| plugin_large.generate(())).collect();
+
+        assert!(std_deviation(&mut small_vals) > std_deviation(&mut large_vals));
+    }
+}
+
+fn std_deviation(data: &mut VecDeque<f32>) -> f32 {
+    let avg = avg(data);
+    let count = data.len();
+    let variance = data
+        .iter()
+        .map(|value| {
+            let diff = avg - (*value as f32);
+            diff * diff
+        })
+        .sum::<f32>()
+        / count as f32;
+    variance.sqrt()
 }
